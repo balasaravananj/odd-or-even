50b3608c523cae451558d98a2b6078c0868c1a0c
balasaravananj <balasaravanan2000@gmail.com>
2023-11-20 12:20:16 +0530

    added dispatch to props

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { startGame, endGame } from '../actions/settings';
 
 
 
@@ -8,7 +9,20 @@ class App extends Component {
     render(){
         return(
             <div>
-                <h1>React Project!</h1>
+                <h1> ♠ ♣ Odd or Even ♥ ♦ </h1>
+                {
+                    this.props.gameStarted ? (
+                        <div>
+                            <h3>The game is on!</h3>
+                            <br />
+                            <button onClick={this.props.endGame}>Cancel Game</button>
+                        </div>
+                     ) : ( <div>
+                        <h3>A new game awaits</h3>
+                        <br />
+                        <button onClick={this.props.startGame}>Start Game</button>
+                         </div>)
+                }
             </div>
         );
     }
@@ -18,8 +32,15 @@ const mapStateToProps = (state) => {
     return {
         gameStarted : state.gameStarted
     };
-}
+};
 
-const componentConnector = connect(mapStateToProps);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        startGame: () => dispatch(startGame()),
+        endGame: () => dispatch(endGame())
+    };
+};
+
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
 
 export default componentConnector(App);
