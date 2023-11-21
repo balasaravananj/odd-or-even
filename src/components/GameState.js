d8d5c62f15e64eab929c807c81ca4e0e56384102
balasaravananj <balasaravanan2000@gmail.com>
2023-11-21 20:41:50 +0530

    updated GameState

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -1,13 +1,32 @@
 import React from "react";
 import {connect} from 'react-redux';
 
+const correctGuessesRecordKey = 'CORRECT_GUESSES_RECORD_1';
+
+const checkRecord = correctGuesses => {
+    const record = Number(localStorage.getItem(correctGuessesRecordKey));
+    if(correctGuesses > record){
+        localStorage.setItem(correctGuessesRecordKey, correctGuesses);
+         return {
+            record : correctGuesses,
+            isNewRecord : true
+         }
+    }
+
+    return {record, isNewRecord : false};
+
+}
 
 const GameState = ({remaining, correctGuesses}) => {
 
     const guessText = correctGuesses === 1 ? 'guess' : 'guesses';
 
+    const {record , isNewRecord} = checkRecord(correctGuesses);
+
+    const recordLabel = isNewRecord ? '🎊 New record' : 'Record';
+
     return (<div>
-        <h3>Record:</h3>
+        <h3>{recordLabel}:{record}</h3>
         <p>Cards remaining:{remaining}</p>
         <p>Number of correct {guessText}:{correctGuesses} </p>
     </div>)
